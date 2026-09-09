92f915f242165bb62db2fce67d6b2c60828d26fa
Olivia <olivia.meisohle@gmx.de>
2026-09-09 16:48:48 +0100

    POST upload route funktioniert -test

--- study_plan_app/script/routes/testSummary.ts
+++ study_plan_app/script/routes/testSummary.ts
@@ -1,21 +1,19 @@
 import fs from "fs";
 import path from "path";
 
-//Run file: npx tsx script/testSummaryRoute.ts
+//Run file: npx tsx script/routes/testSummary.ts
 
-//TODO: updaten - file wird nicht mehr direkt gesendet
 async function testUpload(){
-const fileBuffer = fs.readFileSync(path.join(__dirname, "test_files", "klr_slides.pdf"));
+const fileBuffer = fs.readFileSync(path.join(__dirname, "test_files", "Buckelwal.txt"));
 const formData = new FormData();
-formData.append("file", new Blob([fileBuffer]), "klr_slides.pdf");
-formData.append("courseId", String(5));
+formData.append("file", new Blob([fileBuffer]), "Buckelwal.txt");
 
- const res = await fetch("http://localhost:3000/api/upload/uploadPost", { method: "POST", body: formData });
+ const res = await fetch("http://localhost:3000/api/summary/summaryCreate", { method: "POST", body: formData });
       if (!res.ok) throw new Error("Error occured while processing the file");
       const data = await res.json();
 
       if(data.title && data.summary){
-      console.log("=============TEST UPLOAD==============");
+      console.log("=============TEST SUMMARY==============");
       console.log("TITLE: " + data.title);
       console.log("SUMMARY: " + data.summary);
       console.log("STUDYPLAN: " + data.studyplan)
---
+++ study_plan_app/script/routes/testUpload.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+
+//Run file: npx tsx script/routes/testUpload.ts
+
+async function testUpload(){
+const fileBuffer = fs.readFileSync(path.join(__dirname, "test_files", "Buckelwal.txt"));
+const formData = new FormData();
+formData.append("file", new Blob([fileBuffer]), "Buckelwal.txt");
+formData.append("courseId", String(5));
+
+ const res = await fetch("http://localhost:3000/api/upload/uploadPost", { method: "POST", body: formData });
+      if (!res.ok) throw new Error("Error occured while processing the file");
+      const data = await res.json();
+
+      if(data){
+      console.log("=============TEST UPLOAD==============");
+      console.log("Upload ID: " + JSON.stringify(data.response.upload_id));
+      console.log("File name: " + JSON.stringify(data.response.file_name));
+      console.log("File type: " + JSON.stringify(data.response.mime_type));
+      }
+}
+
+testUpload().catch((err) => {
+  console.error("#Error :", err);
+});
